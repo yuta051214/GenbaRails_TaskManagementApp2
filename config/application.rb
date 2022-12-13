6af54a7458125f9f46a88a095ddf7dc6c2ca8f8d
yuta051214 <ffftrick123@gmail.com>
2022-12-13 17:25:33 +0900

    timezoneをUTCからJSTへ変更

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module TaskManagementApp2
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    #* デフォルトのタイムゾーンを UTC -> JST に変更
+    config.time_zone = 'Asia/Tokyo'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
